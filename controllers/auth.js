781ae3e31785619283800d089034781cd26a65ec
Alix2019 <alix1982@yandex.ru>
2024-10-31 20:26:25 +0300

    fix_controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,25 +37,9 @@ module.exports.login = (req, res, next) => {
   }
 };
 
-// module.exports.createPasswordAdmin = (req, res, next) => {
-//   console.log("нужен пароль");
-//   res.send("нужен пароль");
-// };
-
 module.exports.createPasswordAdmin = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => {
-      console.log('ok');
-      const passwordRes = {
-        password: req.body.password,
-        hash: hash,
-      };
-      res.send(passwordRes);
-    })
-    .catch((err) => {
-      console.log(err);
-      next(err);
-    });
+  console.log("нужен пароль");
+  res.send("нужен пароль");
 };
 
 // module.exports.createUser = (req, res, next) => {
